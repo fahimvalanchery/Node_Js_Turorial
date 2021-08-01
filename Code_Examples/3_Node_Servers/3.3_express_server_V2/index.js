5cf78612df93e24fd0d0db912c740e36bd0ff693
fahimvalanchery <fahimvalanchery1@gmail.com>
2021-08-01 10:46:28 +0530

    get routers

--- Code_Examples/3_Node_Servers/3.3_express_server_V2/index.js
+++ Code_Examples/3_Node_Servers/3.3_express_server_V2/index.js
@@ -5,8 +5,7 @@
 const express = require("express");
 const path = require("path"); //node built in
 
-//
-const data = require("./data");
+
 
 // express app
 const app = express();
@@ -19,71 +18,12 @@ app.get("/", (req, res) => {
     // response with simple Text
     res.send("hello from simple server :)");
 });
-app.get("/home", (req, res) => {
-    // response with Html
-    res.send("<p>home page</p>");
-});
 
-app.get("/about", (req, res) => {
-    // response with Html file
-    res.sendFile("./views/about.html", { root: __dirname });
-});
+const getRouters=require('./src/routers/getmethod.router')
 
-app.get("/aboutnew", function (req, res) {
-    // response with Html file with another code structure
-    res.sendFile(path.join(__dirname + "/views/about.html"));
-});
+app.use('/getmethod',getRouters)
 
-app.get("/about-us", (req, res) => {
-    // redirects to another path
-    res.redirect("/about");
-});
 
-app.get("/users", (req, res) => {
-    // let users=data.users
-    let { users } = data; //same functionality of above code
-    //   check the request contain any query is there or not
-    if (req.query && Object.entries(req.query).length) {
-        // query is not an empty object
-        let { age } = req.query;
-        // check if age is in query object
-        if (age) {
-            let selectedUsers = [];
-            for (let index = 0; index < users.length; index++) {
-                //   checking age is same with query age
-                if (users[index].age == age) {
-                    selectedUsers.push(users[index]);
-                }
-            }
-            res.send({ users: selectedUsers });
-        } else {
-            res.send({ msg: "Invalid query values" });
-        }
-    } else {
-        // there is no query, is  {}
-        res.send({ users });
-    }
-});
-
-app.get("/users/:id", (req, res) => {
-    // let users=data.users
-    let { users } = data; //same functionality of above code
-    // return selected User
-
-    // access id from req
-    let userId = req.params.id;
-    let selected_user = {};
-    for (let index = 0; index < users.length; index++) {
-        if (users[index]._id == userId) {
-            selected_user = users[index];
-            res.send({ selected_user });
-            return; // this return will stop if the user id selected, no nee to loop till last
-        }
-    }
-    //   if it is already returned, then this code will not execute
-    console.log("this will not execute if user is selecetd");
-    res.send({ selected_user });
-});
 
 // 404 page
 // Keep This in always as last route otherways if you add any route in below it will not work
---
+++ Code_Examples/3_Node_Servers/3.3_express_server_V2/src/routers/getmethod.router.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router= express.Router()
+//
+const data = require("../../data");
+
+/**
+ * Get method routes
+ */
+ router.get("/home", (req, res) => {
+    // response with Html
+    res.send("<p>home page</p>");
+});
+
+router.get("/about", (req, res) => {
+    // response with Html file
+    res.sendFile("./views/about.html", { root: __dirname });
+});
+
+router.get("/aboutnew", function (req, res) {
+    // response with Html file with another code structure
+    res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+router.get("/about-us", (req, res) => {
+    // redirects to another path
+    res.redirect("/about");
+});
+
+router.get("/users", (req, res) => {
+    // let users=data.users
+    let { users } = data; //same functionality of above code
+    //   check the request contain any query is there or not
+    if (req.query && Object.entries(req.query).length) {
+        // query is not an empty object
+        let { age } = req.query;
+        // check if age is in query object
+        if (age) {
+            let selectedUsers = [];
+            for (let index = 0; index < users.length; index++) {
+                //   checking age is same with query age
+                if (users[index].age == age) {
+                    selectedUsers.push(users[index]);
+                }
+            }
+            res.send({ users: selectedUsers });
+        } else {
+            res.send({ msg: "Invalid query values" });
+        }
+    } else {
+        // there is no query, is  {}
+        res.send({ users });
+    }
+});
+
+router.get("/users/:id", (req, res) => {
+    // let users=data.users
+    let { users } = data; //same functionality of above code
+    // return selected User
+
+    // access id from req
+    let userId = req.params.id;
+    let selected_user = {};
+    for (let index = 0; index < users.length; index++) {
+        if (users[index]._id == userId) {
+            selected_user = users[index];
+            res.send({ selected_user });
+            return; // this return will stop if the user id selected, no nee to loop till last
+        }
+    }
+    //   if it is already returned, then this code will not execute
+    console.log("this will not execute if user is selecetd");
+    res.send({ selected_user });
+});
+
+module.exports=router
